c2f65f75b21ccdd2555d24c0cbfed60d8c2c8786
Yatima Kagurazaka <Yatima.Kgr@gmail.com>
2017-11-24 15:28:03 +0900

    install Vue Material

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 new Vue({
   el: '#app',
   render: h => h(App)
