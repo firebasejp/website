aaf02a477ae6c541fd82c1df5ef7174f4039e0b2
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-12-12 16:55:20 +0900

    Set hosting header

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './public'),
     publicPath: '/',
-    filename: 'build.js'
+    filename: 'build.js?[chunkhash]'
   },
   module: {
     rules: [
